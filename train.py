dedf484fbae1a18018a09cb73c714ceb90a71031
pranaval <pranav2109@hotmail.com>
2021-11-27 15:05:14 +0530

    Working model

--- train.py
+++ train.py
@@ -9,17 +9,16 @@ import torch.nn.functional as F
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 from torch.utils.data import random_split
-
+from sklearn.preprocessing import LabelEncoder
 
 warnings.filterwarnings("ignore")
 
-EPOCHS = 100
-SEQ_LEN = 10
+EPOCHS = 500
 BATCH_SIZE = 1
 learning_rate = 0.01
-input_size = 28
-hidden_size = 64
-num_layers = 1
+input_size = 6
+hidden_size = [32,64,128,64,32]
+outsize = 10
 
 file_path = "features_to_train.csv"
 
@@ -28,40 +27,25 @@ class CraneData(Dataset):
     def __init__(self, path_to_data):
     
         self.data = pd.read_csv(path_to_data)
-        self.seq_length = SEQ_LEN
-        self.x = []
-        self.y = []
         
+        self.label_encoder()
         self.normalize_columns()
-        self.create_dataset()
-        
+    
+    def label_encoder(self):
+        lb = LabelEncoder()
+        self.data["reward"] = lb.fit_transform(self.data["reward"])
+             
     def normalize_columns(self):
-        
         for cols in self.data.columns[3:-1]:
             self.data[cols] = self.data[cols] / self.data[cols].abs().max()
-            
-    def create_dataset(self):
-        
-        for sess in self.data["Session id"].unique():
-            filter = (self.data["Session id"]==sess)
-            num_of_elements = len(self.data[filter])
-            features = self.data[filter].drop(columns=["reward", "Quality of Action"])
-            labels = self.data[filter]["reward"]
-        
-            for i in range(num_of_elements-self.seq_length-1):
-                x_ = [features.iloc[i,3:].to_list() for i in range(i,i+self.seq_length)]
-                y_ = labels[i:i+self.seq_length].sum()
-                self.x.append(x_)
-                self.y.append(y_)
                 
     def __len__(self):
-    
-        return len(self.x)
+        return len(self.data)
     
     def __getitem__(self, index):
     
-        inp_data = self.x[index]
-        labels = self.y[index]
+        inp_data = self.data.iloc[index][self.data.columns[3:-1]]
+        labels = self.data.iloc[index,-1]
         return torch.tensor(inp_data), torch.tensor(labels)
     
 data = CraneData(file_path)
@@ -75,70 +59,114 @@ print(f"Test Size: {len(test_dataset)}")
 train_loader = DataLoader(train_dataset, shuffle=True, batch_size=BATCH_SIZE)
 test_loader = DataLoader(test_dataset, shuffle=True, batch_size=BATCH_SIZE)
 
-class LSTMNet(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers):
+class Net(nn.Module):
+    def __init__(self, input_size, hidden_size):
         super().__init__()
-        self.num_layers = num_layers
         self.input_size = input_size
         self.hidden_size = hidden_size
-        self.seq_length = SEQ_LEN
-        
-        self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size,
-                            num_layers=num_layers, batch_first=True)
-        
-        self.fc = nn.Sequential(nn.Linear(hidden_size, 32),
-                                nn.ReLU(),
-                                nn.Linear(32, 1))
+        self.out_size = outsize
+                
+        self.fc1 = nn.Sequential(nn.Linear(self.input_size, self.hidden_size[0]),
+                                nn.ReLU())
+        self.droput = nn.Dropout(0.25)
+        self.fc2 = nn.Sequential(nn.Linear(self.hidden_size[0], self.hidden_size[1]),
+                                nn.ReLU())
+        self.fc3 = nn.Sequential(nn.Linear(self.hidden_size[1], self.hidden_size[2]),
+                                nn.ReLU())
+        self.fc4 = nn.Sequential(nn.Linear(self.hidden_size[2], self.hidden_size[3]),
+                                nn.ReLU())
+        self.fc5 = nn.Sequential(nn.Linear(self.hidden_size[3], self.hidden_size[4]),
+                                nn.ReLU())
+        self.fc6 = nn.Linear(self.hidden_size[4], self.out_size)
         
     def forward(self, x):
         
-        h_0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
-        c_0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
-        
-        output, (_, _) = self.lstm(x, (h_0, c_0))
-        output = output[:,-1,:]
-        out = self.fc(output)
+        out = self.fc1(x)
+        out = self.droput(out)
+        out = self.fc2(out)
+        out = self.droput(out)
+        out = self.fc3(out)
+        out = self.droput(out)
+        out = self.fc4(out)
+        out = self.droput(out)
+        out = self.fc5(out)
+        out = self.droput(out)
+        out = self.fc6(out)
         return out
     
-net = LSTMNet(input_size,hidden_size,num_layers)
-criterion = nn.MSELoss()               
+net = Net(input_size,hidden_size)
+criterion = nn.CrossEntropyLoss()               
 optimizer = optim.SGD(net.parameters(), lr=learning_rate)
 tr_loss = []
 te_loss = []
+tr_acc = []
+te_acc = []
+
+
+def multi_acc(y_pred, y_test):
+    y_pred_softmax = torch.log_softmax(y_pred, dim=1)
+    _, y_pred_tags = torch.max(y_pred_softmax, dim=1)
+    correct_pred = (y_pred_tags==y_test).float()
+    acc = correct_pred.sum() /len(correct_pred)
+    acc = torch.round(acc * 100)
+    return acc
 
 for epoch in range(EPOCHS):
     train_loss = 0.0
     test_loss = 0.0
     count = 0
+    train_acc = 0.0
+    test_acc = 0.0
+    
     for indx, (inp, lab) in enumerate(train_loader):
-        inp, lab = inp.float(), lab.float()
-        testinp, testlab = next(iter(test_loader))
+        net.train()
+        inp, lab = inp.float(), lab
         optimizer.zero_grad()
         outputs = net(inp)
         loss = criterion(outputs, lab)
-        
-        toutputs = net(testinp.float())
-        tloss = criterion(toutputs, testlab.float())
+        train_acc += multi_acc(outputs, lab)
         
         loss.backward()
         optimizer.step()
         
         train_loss += loss.item()
-        test_loss += tloss.item()
+        
+        with torch.no_grad():
+            net.eval()
+            testinp, testlab = next(iter(test_loader))
+            toutputs = net(testinp.float())
+            tloss = criterion(toutputs, testlab)
+            tacc = multi_acc(toutputs, testlab)
+        
+            test_loss += tloss.item()
+            test_acc += tacc
                 
         count += 1
 
     print(f"Epoch:{epoch} - Train Loss:{train_loss/count},---------,Val Loss:{test_loss/count}")
+    print(f"Epoch:{epoch} - Train Acc:{train_acc/count},---------,Val Loss:{test_acc/count}")
     tr_loss.append(train_loss/count)
     te_loss.append(test_loss/count)
+    tr_acc.append(train_acc/count)
+    te_acc.append(test_acc/count)
+    
 
 print("Training Complete") 
- 
+
+plt.subplot(121) 
 plt.xlabel("Epochs")
 plt.ylabel("Loss") 
 plt.plot(range(len(tr_loss)),tr_loss, color='red')        
-plt.plot(range(len(te_loss)),te_loss, color='blue') 
-plt.legend(["Train", "Test"])       
+plt.plot(range(len(te_loss)),te_loss, color='blue')
+plt.legend(["TrainLoss", "TestLoss"])       
+
+plt.subplot(122)
+plt.xlabel("Epochs")
+plt.ylabel("Accuracy")
+plt.plot(range(len(tr_acc)),tr_acc, color='green')        
+plt.plot(range(len(te_acc)),te_acc, color='yellow')  
+plt.legend(["Train Acc","TestAcc"])
+
 plt.savefig("output.png")
 plt.show()
 
