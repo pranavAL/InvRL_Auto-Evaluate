55ba2e37d7a0feed436e77ffd2048379a40c5e9e
pranaval <pranav2109@hotmail.com>
2022-01-23 19:14:32 -0500

    Addition of Auxiliary Loss

--- train_LSTM.py
+++ train_LSTM.py
@@ -8,7 +8,6 @@ import pandas as pd
 import torch.nn as nn
 import pytorch_lightning as pl
 import torch.nn.functional as F
-from torch.autograd import Variable
 from torch.utils.data import Dataset, DataLoader
 from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning import Trainer, seed_everything
@@ -16,26 +15,22 @@ from pytorch_lightning import Trainer, seed_everything
 warnings.filterwarnings("ignore")
 
 class CraneDataset(Dataset):
-    def __init__(self, X: np.ndarray, Y: np.ndarray):
+    def __init__(self, X: np.ndarray, Y_recon: np.ndarray, Y_class: np.ndarray):
         self.X = X
-        self.Y = Y
+        self.Y_recon = Y_recon
+        self.Y_class = Y_class
 
     def __len__(self):
         return len(self.X)
 
     def __getitem__(self, index):
-        return self.X[index], self.Y[index]
+        return self.X[index], self.Y_recon[index], self.Y_class[index]
 
 class CraneDatasetModule():
-    def __init__(self, seq_len, batch_size, model_type, num_workers=2):
+    def __init__(self, seq_len, batch_size, num_workers=2):
         self.seq_len = seq_len
         self.batch_size = batch_size
         self.num_workers = num_workers
-        self.X_train = None
-        self.Y_train = None
-        self.X_test = None
-        self.Y_test = None
-        self.model_type = model_type
 
         data_path = os.path.join("datasets","features_to_train.csv")
 
@@ -46,63 +41,67 @@ class CraneDatasetModule():
 
         df.loc[:,train_feats] = (df.loc[:,train_feats] - df.loc[:,train_feats].min())/(df.loc[:,train_feats].max() - df.loc[:,train_feats].min())
 
-        best_sess = [sess for sess in df["Session id"].unique() if df.loc[df["Session id"]==sess,"Current trainee score at that time"].min()==0]
+        sorted_sess = {}
+        for sess in df['Session id'].unique():
+            sorted_sess[sess] = df.loc[df['Session id']==sess,"Current trainee score at that time"].sum()
+        sorted_sess = dict(sorted(sorted_sess.items(), key = lambda x:x[1], reverse=True))
+        sess2rank = {sess:i for i,sess in enumerate(sorted_sess)}
 
-        X_train = []
-        Y_train = []
-        X_test = []
-        Y_test = []
+        X = []
+        Y_recon = []
+        Y_rank = []
 
         for sess in df["Session id"].unique():
             sess_feat = df.loc[df["Session id"]==sess,:]
-            terminate = (2*self.seq_len if self.model_type=="Forced" else self.seq_len)
+            terminate = 2*self.seq_len
             for i in range(0,len(sess_feat) - terminate):
-                if sess in best_sess:
-                    X_train.append(sess_feat.iloc[i:i+self.seq_len,:][train_feats].values)
-                    if self.model_type=="Forced":
-                        Y_train.append(sess_feat.iloc[(i+self.seq_len)-1:(i+(2*self.seq_len))-1,:][train_feats].values)
-                    else:
-                        Y_train = X_train
-                else:
-                    X_test.append(sess_feat.iloc[i:i+self.seq_len,:][train_feats].values)
-                    if self.model_type=="Forced":
-                        Y_test.append(sess_feat.iloc[(i+self.seq_len)-1:(i+(2*self.seq_len))-1,:][train_feats].values)
-                    else:
-                        Y_test = X_test
+                X.append(sess_feat.iloc[i:i+self.seq_len,:][train_feats].values)
+                Y_recon.append(sess_feat.iloc[(i+self.seq_len)-1:(i+(2*self.seq_len))-1,:][train_feats].values)
+                Y_rank.append(sess2rank[sess])
 
-        self.X_train = torch.tensor(X_train).float()
-        self.Y_train = torch.tensor(Y_train).float()
-        self.X_test = torch.tensor(X_test).float()
-        self.Y_test = torch.tensor(Y_test).float()
+        X = np.array(X)
+        Y_recon = np.array(Y_recon)
+        Y_rank = np.array(Y_rank)
+
+        tot_indx = np.random.randint(low=0,high=len(X),size=len(X))
+        train_size = int(0.8 * len(X))
+
+        X_train, X_test = X[tot_indx[:train_size]], X[tot_indx[train_size:]]
+        Y_train_recon, Y_test_recon = Y_recon[tot_indx[:train_size]], Y_recon[tot_indx[train_size:]]
+        Y_train_class, Y_test_class = Y_rank[tot_indx[:train_size]], Y_rank[tot_indx[train_size:]]
+
+        self.X_train, self.X_test = torch.tensor(X_train).float(), torch.tensor(X_test).float()
+        self.Y_train_recon, self.Y_test_recon = torch.tensor(Y_train_recon).float(), torch.tensor(Y_test_recon).float()
+        self.Y_train_class, self.Y_test_class = torch.tensor(Y_train_class), torch.tensor(Y_test_class)
 
     def train_dataloader(self):
-        train_dataset = CraneDataset(self.X_train, self.Y_train)
+        train_dataset = CraneDataset(self.X_train, self.Y_train_recon, self.Y_train_class)
         train_loader = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers)
 
         return train_loader
 
     def test_dataloader(self):
-        test_dataset = CraneDataset(self.X_test, self.Y_test)
+        test_dataset = CraneDataset(self.X_test, self.Y_test_recon, self.Y_test_class)
         test_loader = DataLoader(test_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers)
 
         return test_loader
 
 class Encoder(nn.Module):
-    def __init__(self, n_features, embedding_dim, seq_len, num_layers):
+    def __init__(self, n_features, seq_len, latent_spc, fc_dim):
         super(Encoder, self).__init__()
-        self.num_layers = num_layers
         self.seq_len, self.n_features = seq_len, n_features
-        self.embedding_dim, self.hidden_dim = embedding_dim, 2 * embedding_dim
-        self.latent_spc = int(self.embedding_dim / 2)
+        self.latent_spc = latent_spc
+        self.fc_dim =  fc_dim
 
         self.lstm = nn.LSTM(input_size=self.n_features,
-                             hidden_size=self.hidden_dim,
-                             num_layers=num_layers,
+                             hidden_size=self.n_features,
                              batch_first=True)
 
-        self.fc = nn.Linear(self.hidden_dim, self.embedding_dim)
-        self.fc1 = nn.Linear(self.embedding_dim, self.latent_spc)
-        self.fc2 = nn.Linear(self.embedding_dim, self.latent_spc)
+        self.elu = nn.ELU()
+        self.fc = nn.Linear(self.n_features, self.fc_dim)
+        self.fc1 = nn.Linear(self.fc_dim, self.fc_dim)
+        self.ls1 = nn.Linear(self.fc_dim, self.latent_spc)
+        self.ls2 = nn.Linear(self.fc_dim, self.latent_spc)
 
     def reparameterize(self, mu, logvar):
         std = torch.exp(0.5 * logvar)
@@ -111,143 +110,152 @@ class Encoder(nn.Module):
 
     def forward(self, x):
         _, (h_out, _) = self.lstm(x)
-        out = self.fc(h_out)
-        mu, logvar = self.fc1(out), self.fc2(out)
+        out = self.elu(self.fc(h_out))
+        out = self.elu(self.fc1(out))
+        mu, logvar = self.ls1(out), self.ls2(out)
         z = self.reparameterize(mu, logvar)
         return z, mu, logvar
 
 class Decoder(nn.Module):
-    def __init__(self, input_dim, seq_len, num_layers, n_features, model_type):
+    def __init__(self, seq_len, latent_spc, n_features):
         super(Decoder, self).__init__()
         self.n_features = n_features
-        self.seq_len, self.input_dim = seq_len, input_dim
-        self.model_type = model_type
+        self.seq_len = seq_len
+        self.latent_spc = latent_spc
 
-        self.initial_layer = nn.Linear(self.input_dim, self.n_features)
+        self.initial_layer = nn.Linear(latent_spc, n_features)
+        self.elu = nn.ELU()
 
         self.lstm = nn.LSTM(input_size=self.n_features,
-                             hidden_size=self.n_features,
-                             num_layers=num_layers)
+                             hidden_size=self.n_features)
 
         self.output_layer = nn.Linear(self.n_features, n_features)
 
     def forward(self, x, last_feat):
 
-        if self.model_type=="Forced":
+        out = last_feat.unsqueeze(0)
+        batch_size = out.size()[1]
+        hidden = self.elu(self.initial_layer(x))
+        cell = self.elu(self.initial_layer(x))
+        outputs = torch.zeros(self.seq_len, batch_size, self.n_features).cuda()
 
-            out = last_feat.unsqueeze(0)
-            batch_size = out.size()[1]
+        for i in range(self.seq_len):
+            out, (hidden, cell) = self.lstm(out, (hidden, cell))
+            self.output_layer(out)
+            outputs[i] = out
 
-            hidden = self.initial_layer(x)
-            cell = self.initial_layer(x)
-            outputs = torch.zeros(self.seq_len, batch_size, self.n_features).cuda()
-
-            for i in range(self.seq_len):
-                out, (hidden, cell) = self.lstm(out, (hidden, cell))
-                self.output_layer(out)
-                outputs[i] = out
-
-            outputs = outputs.reshape((batch_size, self.seq_len, self.n_features))
-
-        else:
-            x = self.initial_layer(x)
-            x = x.repeat(self.seq_len, 1, 1)
-            x = x.reshape((-1, self.seq_len, self.n_features))
-
-            out, _ = self.lstm(x)
-            out = out.reshape((-1, self.seq_len, self.n_features))
-            outputs = self.output_layer(out)
+        outputs = outputs.reshape((batch_size, self.seq_len, self.n_features))
 
         return outputs
 
+class Classifier(nn.Module):
+    def __init__(self, latent_spc, fc_dim, n_class):
+        super(Classifier, self).__init__()
+        self.latent_spc = latent_spc
+        self.fc_dim = fc_dim
+        self.n_class = n_class
+
+        self.elu = nn.ELU()
+        self.fc1 = nn.Linear(self.latent_spc, self.fc_dim)
+        self.fc2 = nn.Linear(self.fc_dim, self.fc_dim)
+        self.out = nn.Linear(self.fc_dim, self.n_class)
+
+    def forward(self, x):
+        out = self.elu(self.fc1(x))
+        out = self.elu(self.fc2(out))
+        out = self.out(out)
+
+        return out
+
 class LSTMPredictor(pl.LightningModule):
-    def __init__(self, n_features, embedding_dim, seq_len, batch_size, num_layers, learning_rate, model_type):
+    def __init__(self, n_features, fc_dim, seq_len, batch_size, latent_spc, learning_rate, n_class):
         super(LSTMPredictor,self).__init__()
         self.n_features = n_features
-        self.embedding_dim = embedding_dim
+        self.fc_dim = fc_dim
         self.seq_len = seq_len
         self.batch_size = batch_size
-        self.num_layers = num_layers
         self.learning_rate = learning_rate
-        self.latent_spc = int(embedding_dim / 2)
+        self.latent_spc = latent_spc
+        self.n_class = n_class
 
-        self.encoder = Encoder(n_features, embedding_dim, seq_len, num_layers)
-        self.decoder = Decoder(self.latent_spc, seq_len, self.num_layers, n_features, model_type)
+        self.encoder = Encoder(n_features, seq_len, latent_spc, fc_dim)
+        self.decoder = Decoder(seq_len, latent_spc, n_features)
+        self.classifier = Classifier(latent_spc, fc_dim, n_class)
 
         self.save_hyperparameters()
 
     def forward(self, x):
         last_feat = x[:,-1,:]
         x, mu, logvar = self.encoder(x)
+        rank = self.classifier(x)
         x = self.decoder(x, last_feat)
-        return x, mu, logvar
+        return x, mu, logvar, rank
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=self.learning_rate)
 
     def training_step(self, batch, batch_idx):
-        x, y = batch
-        y_hat, mu, logvar = self(x)
-        rloss = F.mse_loss(y_hat, y)
+        x, y_decod, y_rank = batch
+        y_hat, mu, logvar, y_hat_rank = self(x)
+        y_hat_rank = y_hat_rank.squeeze(0)
+        rloss = F.mse_loss(y_hat, y_decod)
         kld = torch.mean(-0.5 * torch.sum(1 + logvar -mu.pow(2) - logvar.exp(), dim=1), dim=1)
-        loss = rloss + kld
+        bce = nn.CrossEntropyLoss()(y_hat_rank, y_rank)
+        loss = rloss + kld + bce
         self.log('train/recon_loss', rloss, on_epoch=True)
         self.log('train/kld', kld, on_epoch=True)
         self.log('train/total_loss', loss, on_epoch=True)
+        self.log('train/Classification_loss', bce, on_epoch=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
-        x, y = batch
-        y_hat, mu, logvar = self(x)
-        rloss = F.mse_loss(y_hat, y)
+        x, y_decod, y_rank = batch
+        y_hat, mu, logvar, y_hat_rank = self(x)
+        y_hat_rank = y_hat_rank.squeeze(0)
+        rloss = F.mse_loss(y_hat, y_decod)
         kld = torch.mean(-0.5 * torch.sum(1 + logvar -mu.pow(2) - logvar.exp(), dim=1), dim=0)
-        loss = rloss + kld
+        bce = nn.CrossEntropyLoss()(y_hat_rank, y_rank)
+        loss = rloss + kld + bce
         self.log('val/recon_loss', rloss, on_epoch=True)
         self.log('val/kld', kld, on_epoch=True)
+        self.log('val/Classification_loss', bce, on_epoch=True)
         self.log('val/total_loss', loss, on_epoch=True)
         return loss
 
     def test_step(self, batch, batch_idx):
-        x, y = batch
-        y_hat, mu, logvar = self(x)
-        rloss = F.mse_loss(y_hat, y)
+        x, y_decod, y_rank = batch
+        y_hat, mu, logvar, y_hat_rank = self(x)
+        y_hat_rank = y_hat_rank.squeeze(0)
+        rloss = F.mse_loss(y_hat, y_decod)
         kld = torch.mean(-0.5 * torch.sum(1 + logvar -mu.pow(2) - logvar.exp(), dim=1), dim=0)
-        loss = rloss + kld
+        bce = nn.CrossEntropyLoss()(y_hat_rank, y_rank)
+        loss = rloss + kld + bce
         self.log('test/recon_loss', rloss, on_epoch=True)
         self.log('test/kld', kld, on_epoch=True)
+        self.log('test/Classification_loss', bce, on_epoch=True)
         self.log('test/total_loss', loss, on_epoch=True)
         return loss
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Hyperparameter Values")
-    parser.add_argument('-mt', '--model_type', type=str, help="Normal or Forced")
     parser.add_argument('-sq','--seq_len', type=int, help="Sequence Length for input to LSTM")
     parser.add_argument('-bs','--batch_size', type=int, default=8, help="Batch Size")
     parser.add_argument('-me','--max_epochs', type=int, default=100, help="Number of epchs to train")
     parser.add_argument('-nf','--n_features', type=int, default=9, help="Length of feature for each sample")
-    parser.add_argument('-ed','--embedding_dim', type=int, default=4, help="Dimension of Sample Space")
-    parser.add_argument('-nl','--num_layers', type=int, default=1, help="Number of LSTM layers")
+    parser.add_argument('-ls','--latent_spc', type=int,default=64, help='Size of Latent Space')
+    parser.add_argument('-fcd','--fc_dim', type=int, default=256, help="Number of FC Nodes")
     parser.add_argument('-lr','--learning_rate', type=float, default=0.001, help="Neural Network Learning Rate")
+    parser.add_argument('-nc','--num_classes', type=int, default=40, help="Number of users")
     args = parser.parse_args()
 
-    p = dict(
-    seq_len = args.seq_len,
-    batch_size = args.batch_size,
-    max_epochs = args.max_epochs,
-    n_features = args.n_features,
-    embedding_dim = args.embedding_dim,
-    num_layers = args.num_layers,
-    learning_rate = args.learning_rate
-)
     dm = CraneDatasetModule(
-        seq_len = p['seq_len'],
-        batch_size = p['batch_size'],
-        model_type = args.model_type
+        seq_len = args.seq_len,
+        batch_size = args.batch_size
     )
 
-    model_path = os.path.join('save_model',f"{p['seq_len']}seq_lstm_vae_{args.model_type}_.pth")
-    wandb.init(name = f"{p['seq_len']}seq_lstm_vae_Recon_New_seq_{args.model_type}_")
+    model_path = os.path.join('save_model',f"{args.seq_len}seq_lstm_vae_.pth")
+    wandb.init(name = f"{args.seq_len}seq_lstm_vae_Recon_New_seq_")
 
     train_loader = dm.train_dataloader()
     test_loader = dm.test_dataloader()
@@ -255,19 +263,19 @@ if __name__ == "__main__":
     seed_everything(1)
 
     wandb_logger = WandbLogger(project="lit-wandb")
-    trainer = Trainer(max_epochs=p['max_epochs'],
+    trainer = Trainer(max_epochs=args.max_epochs,
                     gpus = 1,
                     logger=wandb_logger,
                     log_every_n_steps=5)
 
     model = LSTMPredictor(
-        n_features = p['n_features'],
-        embedding_dim = p['embedding_dim'],
-        seq_len = p['seq_len'],
-        batch_size = p['batch_size'],
-        num_layers = p['num_layers'],
-        learning_rate = p['learning_rate'],
-        model_type = args.model_type
+        n_features = args.n_features,
+        fc_dim = args.fc_dim,
+        seq_len = args.seq_len,
+        batch_size = args.batch_size,
+        latent_spc = args.latent_spc,
+        learning_rate = args.learning_rate,
+        n_class = args.num_classes
     )
 
     trainer.fit(model, train_loader, test_loader)
