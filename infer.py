130d4e2f8499fa7f000c212707196dc834301da9
pranaval <pranav2109@hotmail.com>
2022-02-06 08:53:46 -0500

    Inference simplified

--- infer.py
+++ infer.py
@@ -14,7 +14,7 @@ import matplotlib.patches as mpatches
 from train_LSTM import LSTMPredictor, args
 
 model_path = os.path.join('save_model', args.model_path)
-df = pd.read_csv(os.path.join("datasets","features_to_train.csv"))
+df = pd.read_csv(os.path.join("datasets","test_df.csv"))
 
 train_feats = ['Bucket Angle','Bucket Height','Engine Average Power','Current Engine Power','Engine Torque', 'Engine Torque Average',
                 'Engine RPM (%)', 'Tracks Ground Pressure Front Left', 'Tracks Ground Pressure Front Right']
@@ -39,16 +39,18 @@ def get_numpy(x):
 
 def get_embeddings(input, label):
     input = input.unsqueeze(0).to(model.device)
-    label = label.unsqueeze(0).to(model.device)
+    label = label.view(1,1,-1).to(model.device)
     recon, mu, _ = model(input, label, is_train=False)
     recon = get_numpy(recon)
     return recon
 
-for sess in df["Session id"].unique()[36:38]:
+y_pred = []
+x_input = []
+n_sessions = len(df["Session id"].unique())
+
+for sess in df["Session id"].unique():
     sess_feat = df.loc[df["Session id"]==sess,:]
     terminate = args.seq_len
-    y_pred = []
-    x_input = []
     for i in range(0,len(sess_feat)-terminate,terminate):
         train = list(sess_feat.iloc[i:i+args.seq_len,:][train_feats].values)
         init_label = list(sess_feat.iloc[i+args.seq_len-1,:][train_feats].values)
@@ -58,11 +60,11 @@ for sess in df["Session id"].unique()[36:38]:
 
 fig, ax = plt.subplots(3,3)
 fig.set_size_inches(15, 10)
-train = np.reshape(np.array(x_input), (len(x_input)*args.seq_len, args.n_features))
-label = np.reshape(np.array(y_pred), (len(y_pred)*args.seq_len, args.n_features))
+train = np.reshape(np.array(x_input), (n_sessions, -1, args.n_features))
+label = np.reshape(np.array(y_pred), (n_sessions, -1, args.n_features))
 for i in range(args.n_features):
-    ax[i//3,i%3].plot(train[:,i], color='r', label="Original")
-    ax[i//3,i%3].plot(label[:,i], color='b', label="Predicted")
+    ax[i//3,i%3].plot(train[2,:,i], color='r', label="Original")
+    ax[i//3,i%3].plot(label[2,:,i], color='b', label="Predicted")
     ax[i//3,i%3].set_title(f"Feature {i}")
 
 lines, labels = fig.axes[-1].get_legend_handles_labels()
